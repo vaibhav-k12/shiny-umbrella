0f1c965b13bdc49813af9812c974c5031cee217c
vaibhav-k12 <vaibhavk3@gmail.com>
2019-12-22 17:55:15 -0500

    Adding header and footer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
+    <div class="header">header</div>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="footer">footer</div>
   </div>
 </template>
 
